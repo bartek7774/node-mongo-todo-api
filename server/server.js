bb880117796975de7e7eca95b488410128f75527
Bartek <bartek@gmail.com>
2017-10-30 19:57:55 +0100

    Add user schema validation

--- server/server.js
+++ server/server.js
@@ -1,35 +1,55 @@
-let mongoose=require('mongoose');
+let mongoose = require('mongoose');
 
-mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp',{
-  useMongoClient:true
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp', {
+  useMongoClient: true
 });
 
-let Todo=mongoose.model('Todo',{
-  text:{
-    type:String
+let Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 2,
+    trim: true
   },
-  completed:{
-    type:Boolean
+  completed: {
+    type: Boolean,
+    default: false
   },
-  completedAt:{
-    type:Number
+  completedAt: {
+    type: Number,
+    default: null
   }
 });
 
-let newTodo=new Todo({
-  text:'Cook dinner'
+let newTodo = new Todo({
+  text: 'true'
 });
 
-let newTodo2=new Todo({
-  text:'Eat launch',
-  completed:false,
-  completedAt:-1
+// newTodo.save().then((doc)=>{
+//   // console.log('Save todo ',doc);
+//   console.log(JSON.stringify(doc,undefined,2));
+// },(e)=>{
+//   console.log('Unable to save todo ',e);
+// });
+
+
+let User = mongoose.model('Users', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 2,
+    trim: true
+  }
 });
 
-newTodo2.save().then((doc)=>{
-  // console.log('Save todo ',doc);
-  console.log(JSON.stringify(doc,undefined,2));
-},(e)=>{
-  console.log('Unable to save todo');
+let user1 = new User({
+  email: '  beka@cosm.pl'
+});
+
+
+user1.save().then(({email:doc}) => {
+  console.log('User saved ',doc);
+}, (e) => {
+  console.log('Unable to save user', e);
 });
