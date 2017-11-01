98bc2073f10ba41c7781622681d0e5eeecafa027
Bartek <bartek@gmail.com>
2017-11-01 18:48:05 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,38 +1,52 @@
-const express=require('express');
-const bodyParser=require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-let {mongoose}=require('./db/mongoose');
-let {User}=require('./models/user');
-let {Todo}=require('./models/todo');
+const { ObjectID } = require('mongodb');
+const { mongoose } = require('./db/mongoose');
+const { User } = require('./models/user');
+const { Todo } = require('./models/todo');
 
-const app=express();
+const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=>{
-  
-  let todo=new Todo({
-    text:req.body.text
+app.post('/todos', (req, res) => {
+
+  let todo = new Todo({
+    text: req.body.text
   });
 
-  todo.save().then((doc)=>{
+  todo.save().then((doc) => {
     res.send(doc);
-  },(e)=>{
-    let {errors:{text:{message:name}}}=e;
+  }, (e) => {
+    let { errors: { text: { message: name } } } = e;
     res.status(400).send(e);
   });
 });
 
-app.get('/todos',(req,res)=>{
-  Todo.find().then((docs)=>{
-    res.send({docs});
-  }).catch((e)=>{
+app.get('/todos', (req, res) => {
+  Todo.find().then((docs) => {
+    res.send({ docs });
+  }).catch((e) => {
     res.status(400).send(e);
   });
 });
 
-app.listen(3000,()=>{
+// GET /todos/1234
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  if (!ObjectID.isValid(id))  return res.status(404).send();
+  
+  Todo.findById(id).then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({todo});
+    }).catch(e => res.status(400).send());
+});
+
+app.listen(3000, () => {
   console.log('Started on port 3000');
 });
 
-module.exports={app};
+module.exports = { app };
