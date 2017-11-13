6aec094855000a0abb42dbd80cd0d5fc057b4966
Bartek <bartek@gmail.com>
2017-11-13 10:36:55 +0100

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -281,3 +281,22 @@ describe('POST /users/login', () => {
       });
   });
 });
+
+describe('DELETE users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).to.not.exist;
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        User.findById(users[0]._id).then((doc) => {
+          expect(doc.tokens).to.have.lengthOf(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
